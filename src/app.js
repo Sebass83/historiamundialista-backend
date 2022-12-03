76d5dc60d994464cd623d74a3c33121451f8c0f5
Sebass83 <ssantoss@gmail.com>
2022-12-03 12:15:45 -0300

    solvin some others bugs :D

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 
 import express  from 'express';
+import {PORT} from './config.js'
 import cors  from 'cors';
 
 const app = express();
 app.use(cors())
 
+
+app.set('PORT', PORT);
 export default app;
 
--- src/config.js
+++ src/config.js
@@ -8,3 +8,5 @@ export const DB_USER = process.env.DB_USER || 'root';
 export const DB_PASSWORD = process.env.DB_PASSWORD || ''
 export const DB_NAME = process.env.DB_NAME || 'mundiales'
 export const DB_PORT = process.env.DB_PORT || 3307
+
+export default PORT; DB_HOST; DB_USER; DB_PASSWORD; DB_NAME; DB_PORT
